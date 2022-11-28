a2763b9c22efb98b601fe869b88241caff68b163
Shreeya Satam <95117831+shreeyasatam@users.noreply.github.com>
2022-11-28 14:07:24 +1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomImage1 = "dice" + randomNumber1 + ".png";
+var randomimageSource1 = "images/" + randomImage1;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomimageSource1 );
+
+var randomNumber2 =Math.floor(Math.random() * 6) + 1;
+var randomImage2 = "dice" + randomNumber2 + ".png";
+var randomImageSource2 ="images/" + randomImage2;
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImageSource2);
+
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩 Player 1 wins";
+
+}
+else if (randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "🚩 Player 2 wins";
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw";
+}
